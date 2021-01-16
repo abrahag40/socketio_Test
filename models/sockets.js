f8ab84753acd932add38fc700dad8c1421fcbf7d
Abraham Hernandez <Abrahag40@gmail.com>
2021-01-15 20:29:04 -0600

    carga

---
+++ models/sockets.js
@@ -0,0 +1,29 @@
+class Sockets {
+
+    constructor( io ){
+        this.io = io;
+        this.socketEvents()
+    }
+
+    socketEvents(){
+        this.io.on("connection", (socket) => {
+
+          socket.on("mensaje-to-server", (data) => {
+            this.io.emit('mensaje-from-server', data)
+          });
+
+          // socket.emit('mensaje-bienvenida', 'Bienvenido al server')
+          // socket.on('mensaje-prueba', (data) => {
+          //     console.log(data);
+          // })
+
+          // socket.emit('mensaje-prueba', {
+          //     msg: 'cliente',
+          //     nombre: 'Pepe pecas'
+          // })
+        });
+    }
+
+}
+
+module.exports = Sockets
